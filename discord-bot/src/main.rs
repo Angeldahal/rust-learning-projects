94f064dce5d7185dfcbda1d00c10e94318992040
angel <angeldahal202@gmail.com>
2025-01-21 22:27:35 +0545

    fix message receiving issues

--- discord-bot/src/main.rs
+++ discord-bot/src/main.rs
@@ -1,4 +1,6 @@
 use std::env;
+use serenity::client::{Client, ClientBuilder};
+use serenity::prelude::GatewayIntents;
 
 use serenity::{
     async_trait,
@@ -32,8 +34,15 @@ struct Handler;
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == HELP_COMMAND{
+        println!("Message kind: {:?} ", msg.kind);
+        println!("Received Message {} : {}", msg.content.len(), msg.content);
+        if msg.content.trim() == HELP_COMMAND{
             if let Err(why) = msg.channel_id.say(&ctx.http, HELP_MESSAGE).await {
+                println!("Error sending message: {:?}", why);
+            }
+        }
+        if msg.content.trim() == "Angel" {
+            if let Err(why) = msg.channel_id.say(&ctx.http, "Yes, your lord is here.").await {
                 println!("Error sending message: {:?}", why);
             }
         }
@@ -47,8 +56,9 @@ impl EventHandler for Handler {
 #[tokio::main]
 async fn main() {
     let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
-    
-     let mut client = Client::new(&token)
+    let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
+
+    let mut client = ClientBuilder::new(&token, intents)
     .event_handler(Handler)
     .await
     .expect("Err creating client");
