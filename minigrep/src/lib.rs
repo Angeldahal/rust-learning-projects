eb06099679da2c3461f5bec0f1a22f354c25e4a1
Angel Dahal <angeldahal202@gmail.com>
2025-07-02 10:41:31 +0000

    removed unused

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -71,10 +71,10 @@ mod tests {
     fn one_result() {
         let query = "duct";
         let contents = "\
-Rust:
-safe, fast, productive.
-Pick three.
-";
+        Rust:
+        safe, fast, productive.
+        Pick three.
+        ";
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
     
@@ -82,10 +82,10 @@ Pick three.
     fn case_insensitive() {
         let query = "rUsT";
         let contents = "\
-Rust:
-Safe, Fast, Productive.
-Pick Three.
-Trust me.";
+        Rust:
+        Safe, Fast, Productive.
+        Pick Three.
+        Trust me.";
         assert_eq!(
             vec!["Rust:", "Trust me."],
             search_case_insensitive(query, contents)
